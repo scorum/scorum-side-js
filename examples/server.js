50b1d7b0818f266b868cda967adf80cbeb6ad737
Sergey Grigorenko <sergey.grigorenko@scorum.com>
2018-04-25 12:57:29 +0300

    Update examples and remove unnecessary ones

--- examples/server.js
+++ examples/server.js
@@ -1,13 +1,11 @@
-var scorum = require('../lib');
+const scorum = require('../lib');
 
 scorum.api.setOptions({ url: 'https://blog-api-dev.scorum.com/' });
 scorum.config.set('address_prefix', 'SCR');
 scorum.config.set('chain_id', 'd3c1f19a4947c296446583f988c43fd1a83818fabaf3454a0020198cb361ebd2');
 
-async function init() {
-  const a = await scorum.api.getProfileAsync('kristie');
-  // const a = await scorum.broadcast.registerAsync('', 'kristie');
-  console.log(a);
-}
-
-init();
+(async () => {
+  const user = await scorum.api.getProfileAsync('kristie');
+  // const newUser = await scorum.broadcast.registerAsync('{privateKey}', 'kristie');
+  console.log(user);
+})();
