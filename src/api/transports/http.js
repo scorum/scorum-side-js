cf540d5f7df6618694a37fae85b93271243b1aaa
Sergey Grigorenko <sergey.grigorenko@scorum.com>
2018-05-11 15:13:24 +0300

    Fix bluebird warning for http transport

--- src/api/transports/http.js
+++ src/api/transports/http.js
@@ -50,12 +50,8 @@ export default class HttpTransport extends Transport {
     const id = data.id || this.id++;
     const params = [api, data.method, data.params];
     jsonRpc(this.options.uri, { method: 'call', id, params }).then(
-      res => {
-        callback(null, res);
-      },
-      err => {
-        callback(err);
-      }
+      res => callback(null, res),
+      err => callback(err),
     );
   }
 }
